5447e5390a38232ef83b4f772f9c9254eb3e8510
My PC <My PC@DESKTOP-IBQBT32>
2021-08-24 07:59:49 +0700

    1

--- BT2008/BT2008/Form1.cs
+++ BT2008/BT2008/Form1.cs
@@ -16,5 +16,40 @@ namespace BT2008
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
