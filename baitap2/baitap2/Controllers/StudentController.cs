e7ddf594349c633ee3e81051e52ac011475f864f
My PC <My PC@DESKTOP-IBQBT32>
2021-08-27 13:07:42 +0700

    baithuchanh

---
+++ baitap2/baitap2/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+       [HttpPost]
+       public ActionResult GetInfo(string ten,string tuoi,string lop)
+        {
+            ViewBag.ten = ten;
+            ViewBag.tuoi = tuoi;
+            ViewBag.lop = lop;
+            return View();
+
+
+        }
+    }
+}
