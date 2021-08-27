d3a1a23801d1af73bbc5d7cb393649a860ff1ac0
My PC <My PC@DESKTOP-IBQBT32>
2021-08-27 08:54:29 +0700

    bt2

---
+++ baitap2/baitap2/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using baitap2.Models;
+
+namespace baitap2.Controllers
+{
+    public class DemoController : Controller
+    {
+        giaiphuongtrinh gpt = new giaiphuongtrinh();
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult hello()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double hesoA, double hesoB)
+        {
+            double x = gpt.Giaiphuongtrinhbacnhat(hesoA, hesoB);
+            ViewBag.nghiepPT = x;
+            return View();
+        }
+    }
+}
---
+++ baitap2/baitap2/Models/giaiphuongtrinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap2.Models
+{
+    public class giaiphuongtrinh
+    {
+        public double Giaiphuongtrinhbacnhat(double a,double b)
+        {
+            double x;
+            x = -b / a;
+            return x;
+        }    
+    }
+}
